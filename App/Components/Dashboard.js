6a35cdfd01de923419e95126899170b8c456249d
Dan Le <dhl1337@gmail.com>
2016-10-08 18:54:35 -0500

    passing data when changing route

---
+++ App/Components/Dashboard.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+import {
+    Text,
+    View,
+    StyleSheet
+} from 'react-native';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> This is the dashboard </Text>
+                <Text> {this.props.userInfo} </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 65,
+        flex: 1
+    },
+    image: {
+        height: 350,
+    },
+    buttonText: {
+        fontSize: 24,
+        color: 'white',
+        alignSelf: 'center'
+    }
+});
+
+export default Dashboard;
