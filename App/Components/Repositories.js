96743dc1f75b6d668be7985a9d56a8f26a012757
Dan Le <dhl1337@gmail.com>
2016-10-08 20:19:45 -0500

    add github repo component

---
+++ App/Components/Repositories.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import {
+    ScrollView,
+    Text,
+    View,
+    TouchableHighlight,
+    StyleSheet
+} from 'react-native';
+import Badge from './Badge';
+import Separator from './helpers/Separator';
+
+class Repositories extends Component {
+    openPage(url) {
+        console.log(url);
+    }
+    render() {
+        const repos = this.props.repos;
+        const list = repos.map((item, index) => {
+            const desc = repos[index].description ? <Text style={styles.description}> {repos[index].description} </Text> : <View></View>
+            return (
+                <View key={index}>
+                    <View style={styles.rowContainer}>
+                        <TouchableHighlight
+                            onPress={this.openPage.bind(this, repos[index].html_url)}
+                            underlayColor='transparent'>
+                            <Text style={styles.name}> {repos[index].name} </Text>
+                        </TouchableHighlight>
+                        <Text style={styles.stars}> Stars: {repos[index].stargazers_count}</Text>
+                        {desc}
+                    </View>
+                    <Separator />
+                </View>
+            )
+        });
+        return (
+            <ScrollView style={styles.container}>
+                <Badge userInfo={this.props.userInfo}/>
+                {list}
+            </ScrollView>
+        )
+    }
+}
+
+Repositories.propTypes = {
+    userInfo: React.PropTypes.object.isRequired,
+    repos: React.PropTypes.array.isRequired
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    rowContainer: {
+        flexDirection: 'column',
+        flex: 1,
+        padding: 10
+    },
+    name: {
+        color: '#48BBEC',
+        fontSize: 18,
+        paddingBottom: 5
+    },
+    stars: {
+        color: '#48BBEC',
+        fontSize: 14,
+        paddingBottom: 5
+    },
+    description: {
+        fontSize: 14,
+        paddingBottom: 5
+    }
+});
+
+export default Repositories;
