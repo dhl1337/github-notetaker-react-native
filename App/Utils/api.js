6204456056f7af6514f3be61a5563b719c805c1c
Dan Le <dhl1337@gmail.com>
2016-10-09 11:57:11 -0500

    add data with firebase rest api

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -6,12 +6,21 @@ const api = {
     },
     getRepos(username) {
         username = username.toLowerCase().trim();
-        var url = `https://api.github.com/users/${username}/repos`;
+        const url = `https://api.github.com/users/${username}/repos`;
         return fetch(url).then(res => res.json());
     },
     getNotes(username) {
         username = username.toLowerCase().trim();
-        var
+        const url = `https://github-note-taker-75c77.firebaseio.com/${username}.json`;
+        return fetch(url).then(res => res.json())
+    },
+    addNotes(username, note) {
+        username = username.toLowerCase().trim();
+        const url = `https://github-note-taker-75c77.firebaseio.com/${username}.json`;
+        return fetch(url, {
+            method: 'post',
+            body: JSON.stringify(note)
+        }).then(res => res.json())
     }
 };
 
